6b11c90ba7b1f4b23bef5762dee3dd1b52219bf3
Craig Slusher <Craig@LCNetS.com>
2016-07-01 14:17:56 -0400

    Minor bug fix to rglob function.
      - Adds proc in front of the rglob definition allowing
        rglob to be recognized as a function correctly.

--- vmd_tcl/molecule_loading.tcl
+++ vmd_tcl/molecule_loading.tcl
@@ -2,30 +2,31 @@
 
 # Creates a recursive glob function
 # source code found here: http://wiki.tcl.tk/2042
-rglob {dirlist globlist} {
-        set result {}
-        set recurse {}
-        foreach dir $dirlist {
-                if ![file isdirectory $dir] {
-                        return -code error "'$dir' is not a directory"
-                }
-                foreach pattern $globlist {
-                        lappend result {*}[glob -nocomplain -directory $dir -- $pattern]
-                }
-                foreach file [glob -nocomplain -directory $dir -- *] {
-                        set file [file join $dir $file]
-                        if [file isdirectory $file] {
-                                set fileTail [file tail $file]
-                                if {!($fileTail eq "." || $fileTail eq "..")} {
-                                        lappend recurse $file
-                                }
-                        }
-                }
+proc rglob {dirlist globlist} {
+    set result {}
+    set recurse {}
+    foreach dir $dirlist {
+        if ![file isdirectory $dir] {
+            return -code error "'$dir' is not a directory"
         }
-        if {[llength $recurse] > 0} {
-                lappend result {*}[rglob $recurse $globlist]
+        foreach pattern $globlist {
+            lappend result {*}[glob -nocomplain -directory $dir -- $pattern]
         }
-        return $result
+        foreach file [glob -nocomplain -directory $dir -- *] {
+            set file [file join $dir $file]
+            if [file isdirectory $file] {
+                set fileTail [file tail $file]
+                if {!($fileTail eq "." || $fileTail eq "..")} {
+                    lappend recurse $file
+                }
+            }
+        }
+    }
+    if {[llength $recurse] > 0} {
+        lappend result {*}[rglob $recurse $globlist]
+    }
+    return $result
+}
 
 # Finds and loads first .pdb and first .dcd file found in the indicated 
 # directories and its subdirectories. If no directories are entered the
